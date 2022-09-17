ec3e072f7ff6f9b8db13a5467a2a183a6b192b4e
TrungTQ <trungtq1991@gmail.com>
2022-09-17 15:34:12 +0700

    Task 4: Upload the Docker Image

--- project-ml-microservice-kubernetes/upload_docker.sh
+++ project-ml-microservice-kubernetes/upload_docker.sh
@@ -5,11 +5,13 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath=trungtq1991/udacity-project4
 
 # Step 2:  
 # Authenticate & tag
+docker login --username=trungtq1991
+docker image tag udacity-project4 $dockerpath
 echo "Docker ID and Image: $dockerpath"
 
 # Step 3:
-# Push image to a docker repository
+docker push $dockerpath
