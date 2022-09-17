f78535f8b09dcec577634c4758beb1ebd72ab873
TrungTQ <trungtq1991@gmail.com>
2022-09-17 16:00:56 +0700

    Task 6: Deploy with Kubernetes and Save Output Logs

--- project-ml-microservice-kubernetes/run_kubernetes.sh
+++ project-ml-microservice-kubernetes/run_kubernetes.sh
@@ -4,15 +4,19 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=trungtq1991/udacity-project4
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run udacitycontainer --image=$dockerpath:latest --port=80
 
 # Step 3:
 # List kubernetes pods
+while [[ $(kubectl get pods udacitycontainer -o 'jsonpath={..status.conditions[?(@.type=="Ready")].status}') != "True" ]]; do
+   sleep 1
+done
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward udacitycontainer 8000:80
